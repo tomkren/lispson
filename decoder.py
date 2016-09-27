8e3d15efc1049432cb770c286fbc2b8b2e560ef7
Tomáš Křen <tomkren@gmail.com>
2016-09-27 16:25:53 +0200

    refactoring (mostly variable names)

--- decoder.py
+++ decoder.py
@@ -4,17 +4,14 @@ import json
 def eval_lispson(lispson, lib=None, output_code=False):
     if lib is None:
         lib = {'native': {}}
-    code, ctx = decode(lispson, lib)
+    code, defs = decode(lispson, lib)
+    def_codes = mk_def_codes(defs, lib)
 
-    mk_def = lib['target']['def']
-    ctx_codes = [mk_def(var, code) for var, code in ctx.items()]
-    ctx_code = '\n'.join(ctx_codes)
-
-    exec(ctx_code, lib['native'])
+    exec('\n'.join(def_codes), lib['native'])
     val = eval(code, lib['native'])
 
     if output_code:
-        return val, code, ctx_codes
+        return val, code, def_codes
     else:
         return val
 
@@ -25,70 +22,59 @@ def decode(lispson, lib):
             lispson = json.loads(lispson)
         except ValueError:
             pass
-    ctx = {}
-    code_str = decode_acc(lispson, lib, ctx)
-    return code_str, ctx
+    defs = {}
+    code_str = decode_acc(lispson, lib, defs)
+    return code_str, defs
 
 
-def decode_acc(json_o, lib, ctx):
+def decode_acc(json_o, lib, defs):
     t = type(json_o)
     if t is list:
-        return decode_list(json_o, lib, ctx)
+        return decode_list(json_o, lib, defs)
     elif t is dict:
-        return decode_dict(json_o, lib, ctx)
+        return decode_dict(json_o, lib, defs)
     elif t is str:
-        handle_def(json_o, lib, ctx)
+        handle_def(json_o, lib, defs)
     return str(json_o)
 
 
-def decode_list(json_list, lib, ctx):
+def decode_dict(json_dict, lib, defs):
+    keys = list(json_dict)
+    if len(keys) == 1:
+        head = keys[0]
+        body = decode_acc(json_dict[head], lib, defs)
+        return lib['target']['lam'](head, body)
+    else:
+        return json.dumps(json_dict)
+
+
+def decode_list(json_list, lib, defs):
     if not json_list:  # Is empty ?
         return '[]'
-    fun = decode_acc(json_list[0], lib, ctx)
+    fun = decode_acc(json_list[0], lib, defs)
     args = json_list[1:]
     if fun == "'":  # Is the function "the quote" ?
         return decode_quote(args)
     if fun in lib['macros']:  # Is the function a macro ?
-        return decode_macro(fun, args, lib, ctx)
-    decoded_args = [decode_acc(o, lib, ctx) for o in args]
+        return decode_macro(fun, args, lib, defs)
+    decoded_args = [decode_acc(o, lib, defs) for o in args]
     if fun == "if":
         return decode_if(decoded_args, lib)
-    handle_def(fun, lib, ctx)  # Handles definitions if needed.
+    handle_def(fun, lib, defs)  # Handles definitions if needed.
     return lib['target']['app'](fun, decoded_args)
-
-
-def decode_dict(json_dict, lib, ctx):
-    keys = list(json_dict)
-    if len(keys) == 1:
-        head = keys[0]
-        body = decode_acc(json_dict[head], lib, ctx)
-        return lib['target']['lam'](head, body)
-    else:
-        return json.dumps(json_dict)
 
 
 def decode_quote(args):
     return json.dumps(args[0] if len(args) == 1 else args)
 
 
-def decode_macro(macro_name, args, lib, ctx):
+def decode_macro(macro_name, args, lib, defs):
     macros = lib['macros']
     macro = macros[macro_name]
     if not callable(macro):
         macro = eval_lispson(macro, lib)
         macros[macro_name] = macro  # Non-pure optimization saving the compiled macro (can be omitted)
-    return decode_acc(macro(*args), lib, ctx)
-
-
-def handle_def(sym, lib, ctx):
-    defs = lib['defs']
-    if sym in defs and sym not in ctx:
-        fun_def = defs[sym]
-        if not isinstance(fun_def, str):
-            ctx[sym] = None  # So it won't recurse forever ..
-            fun_def = decode_acc(fun_def, lib, ctx)
-            defs[sym] = fun_def  # Again non-pure optimization ..
-        ctx[sym] = fun_def
+    return decode_acc(macro(*args), lib, defs)
 
 
 # If needs a special treatment because of if's laziness (we must not evaluate both branches)
@@ -98,28 +84,44 @@ def decode_if(decoded_args, lib):
     else:
         raise ValueError('if must have 3 args', len(decoded_args))
 
+
+def handle_def(sym, lib, defs):
+    lib_defs = lib['defs']
+    if sym in lib_defs and sym not in defs:
+        sym_def = lib_defs[sym]
+        if not isinstance(sym_def, str):
+            defs[sym] = None  # So it won't recurse forever ..
+            sym_def = decode_acc(sym_def, lib, defs)
+            lib_defs[sym] = sym_def  # Again non-pure optimization ..
+        defs[sym] = sym_def
+
+
+def mk_def_codes(defs, lib):
+    mk_def = lib['target']['def']
+    return [mk_def(var, code) for var, code in defs.items()]
+
+
 # TESTS :
 
-
 def test_decode(lib, lispson):
-    val, code_str, ctx_codes = eval_lispson(lispson, lib, output_code=True)
+    val, code_str, def_codes = eval_lispson(lispson, lib, output_code=True)
     print(lispson)
-    if ctx_codes:
-        print(' >> '+'\n    '.join(ctx_codes))
+    if def_codes:
+        print(' >> '+'\n    '.join(def_codes))
     print(' ->', code_str)
     print(' =>', val)
     return val
 
 
-# Main with tests.
-def main():
+def run_tests():
 
     lib = {
         'target': {
             'app': lambda fun, args: fun+'('+(', '.join(args))+')',
             'lam': lambda head, body: '(lambda '+head+': '+body+')',
             'if': lambda p, a, b: a+' if '+p+' else '+b,
-            'def': lambda var, code: var+' = '+code
+            'def': lambda var, code: var+' = '+code,
+            'def_fun': lambda name, head, body: 'def '+name+'('+head+'): return '+body
         },
         'native': {
             'eq': lambda a, b: a == b,
@@ -135,7 +137,8 @@ def main():
             'even': {'n': ['if', ['eq', 'n', 0], True, ['odd', ['sub', 'n', 1]]]},
             'odd': {'n': ['if', ['eq', 'n', 0], False, ['even', ['sub', 'n', 1]]]},
             'factorial': {'n': ['if', ['eq', 'n', 0], 1, ['mul', 'n', ['factorial', ['sub', 'n', 1]]]]},
-            'answer': 42
+            'answer': 42,
+            'foo': ["'", 'bar']
         },
         'macros': {
             'lambda': {"head, body": ["mkv", "head", "body"]},
@@ -183,7 +186,10 @@ def main():
     test(False, ['odd', 42])
     test(False, ['even', 23])
     test(True, ['odd', 23])
+
     test(42, 'answer')
+    test('bar', 'foo')
+
     test(23, ['sub', 'answer', 19])
     test(120, ['factorial', 5])
 
@@ -191,4 +197,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    run_tests()
